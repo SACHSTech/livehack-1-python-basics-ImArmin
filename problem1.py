44847bcb8d8a70f95ca202388d166789cabb7a7a
ArminAwista <armin.awista23@ycdsbk12.ca>
2020-12-07 18:45:02 +0000

    I changed the code.

--- problem1.py
+++ problem1.py
@@ -1,1 +1,21 @@
+'''
+-------------------------------------------------------------------------------------------
+Name: problem1.py
+Purpose: Convert celcius into fahrenheit.
 
+Author: Armin
+
+Created: date 2020-12-07
+-------------------------------------------------------------------------------------------
+'''
+
+print(" Welcome to the Celcius to Fahrenheit converter ")
+
+#get celcius
+celcius = float(input("Enter the temperature in celcius: "))
+
+# compute farhenheit
+fahrenheit = (9/5 * celcius) + 32
+
+# output farhenheit
+print("The temperature " + str(celcius) + " in celcius =" + " the temperature " + str(fahrenheit) + " in fahrenheit.")
